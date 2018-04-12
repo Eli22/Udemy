181f5e6cab25bec070e90f6aee21692e092a261e
unknown <elileite9@gmail.com>
2018-04-11 22:47:13 -0300

    Strings e template strings.

---
+++ Udemy/Curso Javascript/exercicios-js/fundamentos/strings.js
@@ -0,0 +1,17 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4));
+console.log(escola.charAt(5));
+console.log(escola.charCodeAt(3)); //unicode
+console.log(escola.indexOf('3'));
+
+console.log(escola.substring(1));
+//vá do indice 0 e me dê 3 caracteres
+console.log(escola.substring(0,3));
+
+console.log('Escola '.concat(escola.concat("!")));
+console.log(escola.replace(3, 'e'));
+console.log(escola.replace(/\d/, 'e')); //regex
+console.log(escola.replace(/\w/g, 'e')); //regex
+
+console.log('Ana,Maria,Pedro'.split(',')); //array
